28a41cb0a62756cf538105ef3dcd6545bf8d2f47
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-19 15:54:59 -0400

    Add files via upload

---
+++ 001_console.log/003_console.log_with_multiple_line_breaks.js
@@ -0,0 +1,28 @@
+// console.log_with_multiple_line_breaks.js
+
+console.log('Hi Everyone\n\nHappy Scripting');
+
+//-//
+
+console.log('Press Enter to Exit');
+process.stdin.resume();
+process.stdin.once('data', function()
+{
+    process.exit();
+});
+
+//----//
+
+/*
+Hi Everyone
+
+Happy Scripting
+Press Enter to Exit
+*/
+
+// Dedicated to God the Father
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+// https://github.com/ChristopherTopalian
+// https://github.com/ChristopherAndrewTopalian
+// https://sites.google.com/view/CollegeOfScripting
+
