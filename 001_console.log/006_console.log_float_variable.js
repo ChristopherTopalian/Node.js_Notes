7bb5298f1b443b5c179ff523e89f90d8a250a4b0
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-19 16:11:26 -0400

    Add files via upload

---
+++ 001_console.log/006_console.log_float_variable.js
@@ -0,0 +1,28 @@
+// console.log_float_variable.js
+
+let numberOfPeople = 20.82;
+
+console.log(numberOfPeople);
+
+//-//
+
+console.log('Press Enter to Exit');
+process.stdin.resume();
+process.stdin.once('data', function()
+{
+    process.exit();
+});
+
+//----//
+
+/*
+20.82
+Press Enter to Exit
+*/
+
+// Dedicated to God the Father
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+// https://github.com/ChristopherTopalian
+// https://github.com/ChristopherAndrewTopalian
+// https://sites.google.com/view/CollegeOfScripting
+
